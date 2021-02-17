77bad5a76ffef5d263241c23bc91d8937ab851fc
Ignacio Correia <ignacio.correia@algardata.pt>
2021-02-17 23:53:09 +0000

    boilerplate done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+import * as OfflinePluginRuntime from "offline-plugin/runtime";
+import axios from "axios";
 
-import '@fortawesome/fontawesome-pro/js/fontawesome'
-import '@fortawesome/fontawesome-pro/js/solid'
-import '@fortawesome/fontawesome-pro/js/regular'
-import '@fortawesome/fontawesome-pro/js/brands'
+import "@fortawesome/fontawesome-pro/js/fontawesome";
+import "@fortawesome/fontawesome-pro/js/solid";
+import "@fortawesome/fontawesome-pro/js/regular";
+import "@fortawesome/fontawesome-pro/js/brands";
 
-import './index.html';
-import './index.scss';
-import './scripts/script';
+import "./index.html";
+import "./index.scss";
+import "./scripts/script";
 
 OfflinePluginRuntime.install();
--- webpack.common.js
+++ webpack.common.js
@@ -1,63 +1,63 @@
-const path = require('path');
+const path = require("path");
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PreloadWebpackPlugin = require("preload-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/index.js',
-    vendor: './src/vendor.js'
+    main: "./src/index.js",
+    vendor: "./src/vendor.js",
   },
   module: {
     rules: [
       {
         test: /\.txt$/,
-        use: 'raw-loader'
+        use: "raw-loader",
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
-              name: '[name].[ext]',
-              outputPath: 'images/',
-              publicPath: '/images/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "/images/",
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|ttf|otf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-              publicPath: 'fonts/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              publicPath: "fonts/",
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -65,40 +65,40 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src', 'robots.txt'),
-          to: path.resolve(__dirname, 'dist', 'robots.txt')
-        }
-      ]
+          from: path.resolve(__dirname, "src", "robots.txt"),
+          to: path.resolve(__dirname, "dist", "robots.txt"),
+        },
+      ],
     }),
     new HtmlWebpackPlugin({
-      title: 'ztv-home-page',
-      filename: 'index.html',
-      template: './src/index.html',
-      inject: 'head'
+      title: "ztv-home-page",
+      filename: "index.html",
+      template: "./src/index.html",
+      inject: "head",
     }),
     new HtmlWebpackPlugin({
-      title: 'ztv-404-page',
-      filename: '404.html',
-      template: './src/404.html',
-      inject: 'head'
+      title: "ztv-404-page",
+      filename: "404.html",
+      template: "./src/404.html",
+      inject: "head",
     }),
     new PreloadWebpackPlugin({
-      rel: 'preload',
+      rel: "preload",
       as(entry) {
         if (/\.(woff|woff2|ttf|otf)$/.test(entry)) {
-          return 'font';
+          return "font";
         }
       },
       fileWhitelist: [/\.(woff|woff2|ttf|otf)$/],
-      include: 'allAssets'
+      include: "allAssets",
     }),
     new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'defer'
-    })
+      defaultAttribute: "defer",
+    }),
   ],
   externals: {
-    $: 'jquery',
-    jquery: 'jQuery',
-    'window.$': 'jquery'
-  }
+    $: "jquery",
+    jquery: "jQuery",
+    "window.$": "jquery",
+  },
 };
